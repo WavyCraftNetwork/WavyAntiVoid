3bb7c7ab17207b39c55279ac47323ef52d1eeec3
WavyCraftNetwork <42108413+WavyCraftNetwork@users.noreply.github.com>
2024-07-22 11:43:58 -0700

    Update AntiVoid.php

--- src/wavycraft/antivoid/AntiVoid.php
+++ src/wavycraft/antivoid/AntiVoid.php
@@ -14,6 +14,7 @@ class AntiVoid extends PluginBase {
     private static self $instance;
     private $saveManager;
     private $economyManager;
+    private array $allowedWorlds;
 
     public function onLoad() : void{
         self::$instance = $this;
@@ -24,6 +25,7 @@ class AntiVoid extends PluginBase {
         $maxSaves = (int) $this->getConfig()->get("max_saves");
         $this->saveManager = new SaveManager($this->getDataFolder(), $maxSaves);
         $this->economyManager = new EconomyManager($this);
+        $this->allowedWorlds = $this->getConfig()->get("worlds", []);
         $this->getServer()->getPluginManager()->registerEvents(new AntiVoidListener(), $this);
         $this->getServer()->getCommandMap()->register("buysaves", new PurchaseCommand());
     }
@@ -38,5 +40,9 @@ class AntiVoid extends PluginBase {
 
     public function getEconomyManager() : EconomyManager{
         return $this->economyManager;
+    }
+
+    public function getAllowedWorlds() : array{
+        return $this->allowedWorlds;
     }
 }
